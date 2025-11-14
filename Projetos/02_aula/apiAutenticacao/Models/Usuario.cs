a3cef6a099d4cb415f35b1d47839a21ed61c070b
jose.ccna <jose.ccna@gmail.com>
2025-11-13 21:18:11 -0300

    apiUsuario

---
+++ Projetos/02_aula/apiAutenticacao/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿namespace apiAutenticacao.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; } = true;
+
+
+        public Usuario()
+        {
+            DataCadastro = DateTime.Now;
+            Ativo = true;
+        }
+
+        //exemplo tualizado public string senha { get; set; }
+        //public int getId()
+        //{
+        //    return Id;
+        //}
+
+
+
+    }
+}
